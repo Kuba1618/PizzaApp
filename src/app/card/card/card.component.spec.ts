209171b14ff02fd9a4cf0b1199610ce45dcaf399
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-06-12 22:13:24 +0200

    Replace payment button and add backend of transaction

--- src/app/card/card/card.component.spec.ts
+++ src/app/card/card/card.component.spec.ts
@@ -19,7 +19,23 @@ describe('CardComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(CardComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'google-pay-demo-app'`, () => {
+    const fixture = TestBed.createComponent(CardComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('google-pay-demo-app');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(CardComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('google-pay-demo-app app is running!');
   });
 });
+ 
--- src/app/card/card/card.component.ts
+++ src/app/card/card/card.component.ts
@@ -9,6 +9,8 @@ import { OrderService } from '../../services/order.service';
 })
 export class CardComponent implements OnInit {
 
+  title = 'Google Pay Demo';
+
   credentials = this.formBuilder.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     surname: ['', [Validators.required, Validators.minLength(3)]],
@@ -20,6 +22,8 @@ export class CardComponent implements OnInit {
 
   amount: any;
 
+  
+
   constructor(private formBuilder: FormBuilder, private orderService: OrderService) { }
 
   ngOnInit(): void {
@@ -27,5 +31,56 @@ export class CardComponent implements OnInit {
       this.amount = res;
     })
   }
+  paymentRequest: google.payments.api.PaymentDataRequest = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['AMEX', 'VISA', 'MASTERCARD']
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId'
+          }
+        }
+      }
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant'
+    },
+    transactionInfo: {
+      totalPriceStatus: 'FINAL',
+      totalPriceLabel: 'Total',
+      totalPrice: '0.10',
+      currencyCode: 'USD',
+      countryCode: 'US'
+    },
+    callbackIntents: ['PAYMENT_AUTHORIZATION']
+  };
 
+  onLoadPaymentData = (
+    event: Event
+  ): void => {
+    const eventDetail = event as CustomEvent<google.payments.api.PaymentData>;
+    console.log('load payment data', eventDetail.detail);
+  }
+
+  onPaymentDataAuthorized: google.payments.api.PaymentAuthorizedHandler = (
+    paymentData
+    ) => {
+      console.log('payment authorized', paymentData);
+      return {
+        transactionState: 'SUCCESS'
+      };
+    }
+
+  onError = (event: ErrorEvent): void => {
+    console.error('error', event.error);
+  }
 }
