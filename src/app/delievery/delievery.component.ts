97a6d94bdf6a788cd6b785ccfa0a94171ff45e8d
Kuba1618 <jakub.prusicki1.618@gmail.com>
2021-06-15 01:01:38 +0200

    Add form to delievery subpage

--- src/app/delievery/delievery.component.ts
+++ src/app/delievery/delievery.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../services/authentication.service';
 import { OrderService } from '../services/order.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-delievery',
@@ -8,10 +9,18 @@ import { OrderService } from '../services/order.service';
   styleUrls: ['./delievery.component.css']
 })
 export class DelieveryComponent implements OnInit {
+  [x: string]: any;
 
   cartTotal: any;
   isLoggedUser!: boolean;
-  constructor(private authService: AuthenticationService, private orderService: OrderService) { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthenticationService, private orderService: OrderService) { }
+
+  credentials = this.formBuilder.group({
+    city: ['', [Validators.required, Validators.minLength(3)]],
+    street: ['', [Validators.required, Validators.minLength(3)]],
+    houseNumber: ['', [Validators.required, Validators.minLength(3)]],
+  })
+
 
   ngOnInit(): void {
     this.authService.isAuthenticated.subscribe((res) => {
